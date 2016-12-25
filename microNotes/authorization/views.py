79286df17cadceb0945eca66fe5a5f52c4eaf840
Spalexy <alex322p9@gmail.com>
2016-12-25 22:47:01 +0300

    Views login&logout
    Сделал вьюхи login&logout

---
+++ microNotes/authorization/views.py
@@ -0,0 +1,28 @@
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import Http404
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+#        if user is not None:
+        if user:
+            auth.login(request, user)
+            if request.META['HTTP_REFERER']:
+                return HttpResponseRedirect(request.META['HTTP_REFERER'])
+            else:
+                return HttpResponseRedirect("/")
+        else:
+            return render(request, 'index.html', {'erros': 'true'})
+    else:
+#        raise Http404
+        return HttpResponseRedirect("/")
+
+def logout(request):
+    auth.logout(request)
+    if request.META['HTTP_REFERER']:
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    else:
+        return HttpResponseRedirect("/")
